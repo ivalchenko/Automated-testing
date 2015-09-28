1cd61fdba2465052f56902d14e33b1acc286c476
ivalchenko <i.valchenk@hotmail.com>
2015-09-28 10:51:16 +0300

    Added simple csharp calculator.

---
+++ Calculator/Calculator.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    class Calculator
+    {
+        public int ActionIndex(string operation)
+        {
+            if (operation.Contains('+'))
+                return 1;
+
+            if (operation.Contains('-'))
+                return 2;
+
+            if (operation.Contains('*'))
+                return 3;
+
+            if (operation.Contains('/'))
+                return 4;
+
+            return 0;
+        }
+
+        public int MakeOperation(string operation, int action)
+        {
+            int op1, op2;
+
+            switch (action)
+            {
+                case(1):
+                    op1 = Convert.ToInt32(operation.Substring(0, operation.IndexOf('+')));
+                    op2 = Convert.ToInt32(operation.Substring(operation.IndexOf('+'), operation.Length - operation.IndexOf('+')));
+                    return op1 + op2;
+                    break;
+
+                case (2):
+                    op1 = Convert.ToInt32(operation.Substring(0, operation.IndexOf('-')));
+                    op2 = Convert.ToInt32(operation.Substring(operation.IndexOf('-') + 1, operation.Length - operation.IndexOf('-') - 1));
+                    return op1 - op2;
+                    break;
+
+                case (3):
+                    op1 = Convert.ToInt32(operation.Substring(0, operation.IndexOf('*')));
+                    op2 = Convert.ToInt32(operation.Substring(operation.IndexOf('*') + 1, operation.Length - operation.IndexOf('*') - 1));
+                    return op1 * op2;
+                    break;
+
+                case (4):
+                    op1 = Convert.ToInt32(operation.Substring(0, operation.IndexOf('/')));
+                    op2 = Convert.ToInt32(operation.Substring(operation.IndexOf('/') + 1, operation.Length - operation.IndexOf('/') - 1));
+                    return op1 / op2;
+                    break;
+            }
+
+            return 0;
+        }
+    }
+}
---
+++ Calculator/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string operation = "";
+            Calculator calculator = new Calculator();
+
+            System.Console.WriteLine("Hello now you start to use calculator by ivalchenko. Please, enter operation:");
+            while (!operation.Equals("stop"))
+            {
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                System.Console.Write("YOU: ");
+                Console.ResetColor();
+
+                try
+                {
+                    operation = Convert.ToString(Console.ReadLine());
+                }
+                catch (System.FormatException e)
+                {
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.WriteLine("\nSYSTEM FORMAT EXCEPTION: ");
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.Write(e.StackTrace);
+                    Console.ResetColor();
+
+                    Console.ForegroundColor = ConsoleColor.Cyan;
+                    Console.WriteLine("\n\nTap to continue...");
+                    Console.ReadKey(true);
+                }
+
+                Console.ForegroundColor = ConsoleColor.Cyan;
+                System.Console.Write("Answer: ");
+                Console.ResetColor();
+
+                if (calculator.MakeOperation(operation, calculator.ActionIndex(operation)) == 0)
+                    System.Console.WriteLine("Invalid operation! Plaese, tyr again.");
+                else
+                    System.Console.WriteLine(calculator.MakeOperation(operation, calculator.ActionIndex(operation)));
+            }
+
+            Console.ReadKey(true);
+        }
+    }
+}
