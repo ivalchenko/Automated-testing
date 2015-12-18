fa9b5177fd1d63162164703d26decca447d7261b
ivalchenko <i.valchenk@hotmail.com>
2015-12-19 01:36:30 +0300

    Added new version of my calculator based on abstruct class.

---
+++ Calculator/Calculator/CalculatorClass.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    public static class CalculatorClass
+    {
+        public static int Calculate(string operation)
+        {
+            int operand1 = 0, operand2 = 0;
+
+            if (operation.Contains('+'))
+            {
+                SetValueToOperands(operation, '+', ref operand1, ref operand2);
+                return operand1 + operand2;
+            }
+
+            if (operation.Contains('-'))
+            {
+                SetValueToOperands(operation, '-', ref operand1, ref operand2);
+                return operand1 - operand2;
+            }
+
+            if (operation.Contains('*'))
+            {
+                SetValueToOperands(operation, '*', ref operand1, ref operand2);
+                return operand1 * operand2;
+            }
+
+            if (operation.Contains('/'))
+            {
+                SetValueToOperands(operation, '/', ref operand1, ref operand2);
+                return operand1 / operand2;
+            }
+
+
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("\nERROR: Invalid operation!");
+            Console.ResetColor();
+            return 0;
+        }
+
+        private static void SetValueToOperands(string operation, char operationSymbol, ref int operand1, ref int operand2)
+        {
+            try
+            {
+                operand1 = Convert.ToInt32(operation.Substring(0, operation.IndexOf(operationSymbol)));
+                operand2 = Convert.ToInt32(operation.Substring(operation.IndexOf(operationSymbol) + 1, operation.Length - operation.IndexOf(operationSymbol) - 1));
+            }
+            catch (FormatException e)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("\nERROR:");
+                Console.ResetColor();
+                Console.Write(e + "\n");
+            }
+        }
+    }
+}
---
+++ Calculator/Calculator/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("SIMPLE CSHARP CALCULATOR CREATED BY ILIA VALCHENKO:");
+            Console.WriteLine("Enter 'stop' if you want to terminate the process.");
+
+            while (true)
+            {
+                string operation;
+
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.Write("\n\nInput: ");
+                Console.ResetColor();
+
+                try
+                {
+                    operation = Convert.ToString(Console.ReadLine());
+                }
+                catch (FormatException e)
+                {
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.WriteLine("\nSYSTEM FORMAT EXCEPTION: ");
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.Write(e.StackTrace);
+                    Console.ResetColor();
+
+                    continue;
+                }
+
+                if (operation == "stop")
+                    break;
+
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.Write("Result: ");
+                Console.ResetColor();
+                Console.Write(CalculatorClass.Calculate(operation));
+            }
+
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("\nTap to continue...");
+            Console.ReadKey(true);
+        }
+    }
+}
