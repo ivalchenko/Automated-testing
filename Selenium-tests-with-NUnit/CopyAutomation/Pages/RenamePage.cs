aa74a7ed276a17b8c52fbbdec55240c9e9358191
ivalchenko <i.valchenk@hotmail.com>
2015-12-20 21:05:34 +0300

    Added test project for Lab#4 with Selenium WebDriver.

---
+++ Selenium-tests-with-NUnit/CopyAutomation/Pages/RenamePage.cs
@@ -0,0 +1,52 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.PageObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitHubAutomation.Pages
+{
+    class RenamePage
+    {
+        private const string BASE_URL = "https://www.copy.com/browse/a:7e08130;z:copy;b:myfiles/Math_For_Alex.rar%3Boid%3A1";
+        private IWebDriver driver;
+
+        [FindsBy(How = How.XPath, Using = "//input[@value='Math_For_Alex.rar']")]
+        private IWebElement inputNameFile;
+
+        [FindsBy(How = How.XPath, Using = "//button[@class='button button-small button-primary has-glyph ']")]
+        private IWebElement butttonRename;
+
+        /*[FindsBy(How = How.XPath, Using = "//li[@class='browser-row type-file subtype-file state-selected-multi state-selected multi-select-eligible']/h1/a")]
+        private IWebElement fileName;*/
+
+        public RenamePage(IWebDriver driver)
+        {
+            this.driver = driver;
+            PageFactory.InitElements(this.driver, this);
+        }
+
+        public void OpenPage()
+        {
+            driver.Navigate().GoToUrl(BASE_URL);
+        }
+
+        public void Rename(string newName)
+        {
+            inputNameFile.Clear();
+            inputNameFile.SendKeys(newName);
+            butttonRename.Click();
+        }
+
+        public string GetFileName()
+        {
+            Console.WriteLine("Into GET FILE NAME method.");
+            string elem = driver.FindElement(By.XPath("//li[@class='browser-row type-file subtype-file  state-selected-multi state-selected multi-select-eligible']/h1/a")).Text;
+            Console.WriteLine("Name is: {0}", elem);                
+
+            return elem;
+        }
+    }
+}
---
+++ Selenium-tests-with-NUnit/CopyAutomation/Tests/SmokeTests.cs
@@ -0,0 +1,74 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium.Support;
+using OpenQA.Selenium.Firefox;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+
+namespace GitHubAutomation
+{
+    [TestFixture]
+    public class SmokeTests
+    {
+        private Steps.Steps steps = new Steps.Steps();
+        private const string USERNAME = "i.valchenk@hotmail.com";
+        private const string PASSWORD = "Self_Pity_11";
+        private const string NEWNAME = "_TestName.rar";
+
+        [SetUp]
+        public void Init()
+        {
+            steps.InitBrowser();
+        }
+
+        [TearDown]
+        public void Cleanup()
+        {
+            steps.CloseBrowser();
+        }
+
+        [Test]
+        public void OneCanLoginCopy()
+        {
+            steps.LoginCopy(USERNAME, PASSWORD);
+            Assert.True(steps.IsLoggedIn(USERNAME));
+        }
+
+        [Test]
+        public void OneCanCreateFolder()
+        {
+            steps.LoginCopy(USERNAME, PASSWORD);
+            Assert.IsTrue(steps.CreateNewFolder("testFolder"));
+        }
+
+        [Test]
+        public void OneCanShareFile()
+        {
+            steps.LoginCopy(USERNAME, PASSWORD);
+            steps.ShareFile(USERNAME, "Hi, it's " + USERNAME + " checkout this archive.");
+            Assert.True(steps.HasUserAccess(USERNAME));
+        }
+
+        [Test]
+        public void OneCanRenameFile()
+        {
+            Console.WriteLine("Start RENAME test.");
+
+            steps.LoginCopy(USERNAME, PASSWORD);
+            steps.RenameFile(NEWNAME);
+            Assert.True(steps.WasFileRenamed(NEWNAME));
+        }
+
+        [Test]
+        public void OneCanLogout()
+        {
+            steps.LoginCopy(USERNAME, PASSWORD);
+            steps.Logout();
+            Assert.True(steps.WasLogout());
+        }
+
+    }
+}
